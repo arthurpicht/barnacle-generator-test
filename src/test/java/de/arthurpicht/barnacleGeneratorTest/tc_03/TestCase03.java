295129aaef8a5d79b4d521b54eddf3abb8d01273
Arthur Picht <picht@arthurpicht.de>
2023-02-15 20:34:29 +0100

    testCase 3

---
+++ src/test/java/de/arthurpicht/barnacleGeneratorTest/tc_03/TestCase03.java
@@ -0,0 +1,48 @@
+package de.arthurpicht.barnacleGeneratorTest.tc_03;
+
+import de.arthurpicht.barnacle.GeneratorInit;
+import de.arthurpicht.barnacleGeneratorTest.TestConfiguration;
+import de.arthurpicht.barnacleGeneratorTest.utils.CleanUp;
+import de.arthurpicht.barnacleGeneratorTest.utils.Prepare;
+import de.arthurpicht.barnacleGeneratorTest.utils.TestPaths;
+import org.junit.jupiter.api.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class TestCase03 {
+
+    private static final String testCase = "tc_03";
+
+    @BeforeAll
+    public static void prepare() throws IOException {
+        CleanUp.cleanUp(testCase);
+        Prepare.prepare(testCase);
+    }
+
+    @AfterAll
+    public static void cleanUp() {
+        if (TestConfiguration.cleanUpAfterEachExecution)
+            CleanUp.cleanUp(testCase);
+    }
+
+    @Test
+    @Order(1)
+    public void generateSchema() throws IOException {
+        System.setProperty(
+                "barnacle.conf",
+                TestPaths.getBarnacleConf(testCase));
+        GeneratorInit.main(null);
+
+        String sql = Files.readString(
+                Paths.get(TestPaths.getSql(testCase)));
+        String sqlExpected = Files.readString(
+                Paths.get(TestPaths.getSqlExpected(testCase)));
+        assertEquals(sqlExpected, sql);
+    }
+
+}
